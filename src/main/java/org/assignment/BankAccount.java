c2856b5de45717f8edb6adc355a3986fd7451655
Rasheeda <krashee@thoughtworks.com>
2025-01-04 23:33:52 +0530

    Add BankAccount class with name and balance properties

---
+++ src/main/java/org/assignment/BankAccount.java
@@ -0,0 +1,20 @@
+package org.assignment;
+
+public class BankAccount {
+
+    private final String name;
+    private final double balance;
+
+    public BankAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
