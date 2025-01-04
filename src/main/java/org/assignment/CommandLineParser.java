a8fbd4a7545e08a3b11916c89bc45d5984c99929
Rasheeda <krashee@thoughtworks.com>
2025-01-04 23:57:15 +0530

    Extract method to create an account for login command

--- src/main/java/org/assignment/CommandLineParser.java
+++ src/main/java/org/assignment/CommandLineParser.java
@@ -7,16 +7,19 @@ public class CommandLineParser {
     String process(String input) {
         String[] inputStrings = input.split(" ");
         String command = inputStrings[0];
+
         return switch (command) {
-            case "login" -> {
-                BankAccount account = new BankAccount(inputStrings[1], 0.0d);
-                yield String.format("Hello, %s!", account.getName());
-            }
+            case "login" -> createAccount(inputStrings[1]);
             case "deposit" -> new Deposit().execute();
             case "withdraw" -> new Withdraw().execute();
             case "transfer" -> new Transfer().execute();
             case "logout" -> "logout";
             default -> "Invalid command";
         };
+    }
+
+    private String createAccount(String accountName) {
+        BankAccount account = new BankAccount(accountName, 0.0d);
+        return String.format("Hello, %s!", account.getName());
     }
 }
