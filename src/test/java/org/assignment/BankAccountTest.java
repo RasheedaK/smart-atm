13ca73e3725b618c06e52c8e8b6f2e73619ac918
Rasheeda <krashee@thoughtworks.com>
2025-01-05 13:05:34 +0530

    Add tests for Deposit and Withdraw methods of BankAccount class

--- src/test/java/org/assignment/BankAccountTest.java
+++ src/test/java/org/assignment/BankAccountTest.java
@@ -21,4 +21,21 @@ class BankAccountTest {
 
         assertFalse(account1.equals(account2));
     }
+
+    @Test
+    void shouldDepositAmountToTheAccount() {
+        BankAccount account = new BankAccount("Alice", 0.0d);
+        account.deposit(100.0d);
+
+        assertEquals(100.0d, account.getBalance());
+    }
+
+    @Test
+    void shouldWithdrawAmountFromTheAccount() {
+        BankAccount account = new BankAccount("Alice", 0.0d);
+        account.deposit(50.0d);
+        account.withdraw(50.0d);
+
+        assertEquals(0.0d, account.getBalance());
+    }
 }
