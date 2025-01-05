15ac0a4e137f309b2da2a5a4ba354d5b7eae4fb7
Rasheeda <krashee@thoughtworks.com>
2025-01-05 20:32:54 +0530

    Add unit test for transfer

--- src/test/java/org/assignment/BankAccountTest.java
+++ src/test/java/org/assignment/BankAccountTest.java
@@ -23,4 +23,16 @@ class BankAccountTest {
 
         assertEquals(0.0d, account.getBalance());
     }
+
+    @Test
+    void shouldTransferAmountFromAccountToAccount() {
+        BankAccount fromAccount = new BankAccount("Alice", 0.0d);
+        BankAccount toAccount = new BankAccount("Bob", 0.0d);
+
+        fromAccount.deposit(50.0d);
+        fromAccount.transfer(toAccount, 50.0d);
+
+        assertEquals(0.0d, fromAccount.getBalance());
+        assertEquals(50.0d, toAccount.getBalance());
+    }
 }
