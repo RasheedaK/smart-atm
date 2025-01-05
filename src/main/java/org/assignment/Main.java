30358061f7e18c070a3ee4e0d5870563944f43c6
Rasheeda <krashee@thoughtworks.com>
2025-01-05 23:35:35 +0530

    Add login to Main method

--- src/main/java/org/assignment/Main.java
+++ src/main/java/org/assignment/Main.java
@@ -1,7 +1,32 @@
 package org.assignment;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ATMClient atmClient = new ATMClient(new ATM(), new Bank());
+
+        start(atmClient);
+    }
+
+    public static void start(ATMClient atmClient) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        System.out.println("Smart ATM started. Type 'exit' to quit.");
+
+        while (true) {
+            System.out.print("Enter command: ");
+            input = scanner.nextLine();
+
+            if ("exit".equalsIgnoreCase(input)) {
+                System.out.println("Exiting program.");
+                break;  // Exit the loop and end the program
+            }
+
+            System.out.println(atmClient.process(input));
+        }
+
+        scanner.close();
     }
 }
