4bdd497ef273a80ed5d3f532d671ee6706c05f66
Rasheeda <krashee@thoughtworks.com>
2025-01-05 13:28:18 +0530

    Add test for exception handling

---
+++ src/test/java/org/assignment/command/WithdrawTest.java
@@ -0,0 +1,23 @@
+package org.assignment.command;
+
+import org.assignment.BankAccount;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class WithdrawTest {
+
+    @Test
+    void shouldThrowExceptionIfBalanceIsLessThanTheAccountToWithdraw() {
+        BankAccount account = new BankAccount("Alice", 0.0d);
+        account.deposit(50.0d);
+
+        Withdraw withdraw = new Withdraw(account, 100.0d);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, withdraw::execute);
+
+        assertEquals("Account balance is low, hence cannot withdraw given amount", exception.getMessage());
+    }
+
+}
